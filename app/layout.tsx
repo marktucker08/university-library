44222ac4a84b19ad370c4276171f86608d0c96cb
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2025-01-24 16:52:21 -0700

    added neon postgresql db and connected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 import localFont from "next/font/local";
 import { ReactNode } from "react";
@@ -43,6 +44,8 @@ const RootLayout = ( {children} : {children: ReactNode;}) => {
         className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
       >
         {children}
+
+        <Toaster />
       </body>
     </html>
   );
--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import Image from "next/image";
+import config from "@/lib/config";
+
+const authenicator = async () => {
+    try {
+        const response = await fetch("http://localhost/3000/api/auth/imagekit");
+
+        if (!response.ok) {
+            const errorText = await response.text();
+
+            throw new Error(
+                 `Request failed with status: ${response.status}: ${errorText}`,
+            );
+        }
+
+        const data = await response.json();
+
+        const { signature, expire, token } = data;
+
+        return {token, expire, signature};
+    }  catch (error:any) {
+        throw new Error(`Authentication request failed: ${error.message}`);
+    }
+};
 
 const ImageUpload = () => {
     return (
-        <div>ImageUpload</div>
+        <button className="upload-btn">
+            <Image src="/icons/upload.svg" alt="upload-icon" width={20} height={20} className="object-contain" />
+
+            <p className="text-base text-light-100">Upload a File</p>
+
+            {/* {file && <p className="upload-filename">{file.filePath}</p>} */}
+        </button>
     );
 };
 
