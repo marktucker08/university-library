b43651cc4949928dc422d6fd433cb9a87b123813
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2025-01-27 16:54:01 -0700

    added schema and generated db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,14 @@ import {Button} from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
-const Home = () => (
+const Home = async () => {
+  const result = await db.select().from(users);
+  console.log(JSON.stringify(result, null, 2));
+  
+    return(
     <>
 
       <BookOverview {... sampleBooks[0]} />
@@ -17,5 +23,5 @@ const Home = () => (
 
     </>
   );
-
+};
 export default Home;
--- database/schema.ts
+++ database/schema.ts
@@ -1,27 +1,21 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { varchar, integer, pgTable, pgEnum, text, boolean, uuid, date, timestamp } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
+export const STATUS_ENUM = pgEnum('status', ["PENDING", "APPROVED", "REJECTED"]);
+export const ROLE_ENUM = pgEnum('role', ["USER", "ADMIN"]);
+export const BORROW_STATUS = pgEnum("borrow_status", ['BORROWED', 'RETURNED']);
+
+
+export const users = pgTable('users', {
+  id: uuid('id').notNull().primaryKey().defaultRandom().unique(),
+  fullName: varchar("full_name", {length: 255}).notNull(),
+  email: text("email").notNull().unique(),
+  universityId: integer("university_id").notNull().unique(),
+  password: text("password").notNull(),
+  universityCard: text("university_card").notNull(),
+  status: STATUS_ENUM("status").default('PENDING'),
+  role: ROLE_ENUM('role').default("USER"),
+  lastActivityDate: date('last_activity_date').defaultNow(),
+  createdAt: timestamp('created_at', {withTimezone: true,}).defaultNow(),
+
 });
 
-export const postsTable = pgTable('posts_table', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
-
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
