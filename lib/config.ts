87dfa84f185d22e469dc94d16fd6cab68814091d
Mark Tucker <markdaddy@gmail.com>
2025-02-03 19:42:59 -0700

    added upstash qstash onboarding workflow

--- lib/config.ts
+++ lib/config.ts
@@ -2,8 +2,10 @@ const config = {
     env: {
         databaseUrl: process.env.DATABASE_URL!,
         upstash: {
-            redisUrl: process.env.UPSTASH_REDIS_URL,
-            redisToken: process.env.UPSTASH_REDIS_TOKEN,
+            redisUrl: process.env.UPSTASH_REDIS_URL!,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN!,
+            qstashUrl: process.env.QSTASH_URL!,
+            qstashToken: process.env.QSTASH_TOKEN!,
         }
     },
 };
