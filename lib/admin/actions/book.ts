5d1221a4594907a97dae9d5d1af18f45dba1a4f6
Mark Tucker <markdaddy@gmail.com>
2025-02-06 16:52:46 -0700

    built book validations and schema

---
+++ lib/admin/actions/book.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import { books } from "@/database/schema";
+import { db } from "@/database/drizzle";
+
+export const createBook = async (params: BookParams) => {
+    try {
+        const newBook = await db
+        .insert(books)
+        .values({
+            ...params,
+            availableCopies: params.totalCopies,
+        })
+        .returning();
+
+        return { 
+            success: true,
+            data: JSON.parse(JSON.stringify(newBook[0])), 
+        };
+    } catch (error) {
+        console.log(error, "Create book error");
+        return { success: false, error: "Create book error" };
+    }
+}
--- types.d.ts
+++ types.d.ts
@@ -21,3 +21,15 @@ interface AuthCredentials {
     universityId: number;
     universityCard: string;
 }
+
+interface BookParams {
+    title: string;
+    author: string;
+    genre: string;
+    rating: number;
+    coverUrl: string;
+    coverColor: string;
+    videoUrl: string;
+    summary: string;
+    totalCopies: number;
+}
